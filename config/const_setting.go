41dee6e3a3fc3d53b98136b85023524c9b7f8e44
xyang76@hawk.iit.edu <xyang76@hawk.iit.edu>
2026-01-08 03:59:59 -0600

    updates

--- config/const_setting.go
+++ config/const_setting.go
@@ -33,6 +33,7 @@ var HeartBeatTimeout *int = flag.Int("ht", 10000, "Heartbeat timeout in millisec
 var BalanceInterval *int = flag.Int("bi", 2000, "Balance interval in milliseconds")
 var TokenRegenerate *int = flag.Int("to", 5000, "Token regeneration in milliseconds")
 var ReplyReceiveTimeout *int = flag.Int("rrt", 10000, "Since leader may crash, lost connection, we need this timeout to count elapse")
+var BatchSize *int = flag.Int("mb", 100, "max batch size")
 
 const CHAN_BUFFER_SIZE = 5000
 const LOG_SIZE = 512 * 1024
@@ -43,7 +44,8 @@ const FALSE = uint8(0)
 var TickTime *int = flag.Int("tt", 5, "Tick time in milliseconds")
 var Commit *int = flag.Int("cmt", 1, "Tick time in milliseconds")
 
-const MAX_BATCH = 10
+var MAX_BATCH = 100
+
 const Max_CMD_SECOND = 50000 //
 const Read_Local = true      //For raft/shipyard, we do not need replicate reads
 const Fail_Prone = false     // Need reconnection
@@ -79,6 +81,7 @@ var CurrentApproach Approach
 func SetEnvironment() {
 	CurrentInstance = Instance(*role)
 	CurrentApproach = Approach(*app)
+	MAX_BATCH = *BatchSize
 }
 
 // electionTimeout generates a pseudo-random election timeout duration.
